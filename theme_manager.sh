6059b253f5a03c881e006c3f545ad4db1680406c
Atticuszz <1831768457@qq.com>
2024-11-02 17:58:55 +0800

    Add theme manager

---
+++ theme_manager.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Define repository list and corresponding installation commands
+declare -A repos=(
+    ["WhiteSur-gtk-theme"]="./install.sh --libadwaita -c Dark -t blue --gnome-shell --round && ./tweaks.sh -F -f --dash-to-dock --color Dark --theme blue && sudo ./tweaks.sh -g -N"
+    ["WhiteSur-icon-theme"]="./install.sh -a -b"
+    ["McMojave-cursors"]="sudo ./install.sh"
+)
+
+# Function to install or update
+process_repos() {
+    local action=$1
+    for repo in "${!repos[@]}"; do
+        if [ "$action" = "install" ] && [ ! -d "$repo" ]; then
+            git clone "https://github.com/vinceliuice/$repo.git"
+        elif [ "$action" = "update" ] && [ -d "$repo" ]; then
+            cd "$repo" || exit 0
+            git pull
+            cd ..
+        fi
+        if [ -d "$repo" ]; then
+            cd "$repo" || exit 0
+            eval "${repos[$repo]}"
+            cd ..
+        fi
+    done
+}
+
+# Check command line arguments
+if [ "$1" = "--install" ]; then
+    process_repos "install"
+elif [ "$1" = "--update" ]; then
+    process_repos "update"
+else
+    echo "Usage: $0 [--install|--update]"
+    echo "  --install: Clone repositories and install"
+    echo "  --update: Update existing repositories and reinstall"
+    exit 1
+fi
